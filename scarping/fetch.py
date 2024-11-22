803ec1a528886493e54fe6753c8645d3646abfcf
Patryk <pat.wawrzonek@gmail.com>
2024-11-22 15:04:30 +0100

    Save to CSV

--- scarping/fetch.py
+++ scarping/fetch.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-
+import random
+import csv
 chromedriver_autoinstaller.install()
 
 chrome_options = Options()
@@ -18,11 +19,20 @@ try:
     driver.get("https://justjoin.it/")
     print("Otwarta strona:", driver.title)
     time.sleep(5)
-   
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": """
+    Object.defineProperty(navigator, 'webdriver', {
+      get: () => undefined
+    });
+    """
+})
+
     accept_cookie_button = driver.find_element(By.ID,"cookiescript_accept")
     accept_cookie_button.click()
     last_index = -1
     current_index = 0
+    if_offer =0
+    data = []
     while True:
         try:
 
@@ -35,7 +45,7 @@ try:
             action.move_to_element_with_offset(element, 0, 50).click().perform() 
             
            
-            time.sleep(2)
+            time.sleep(random.uniform(1.5, 3.5))
 
             try:
                 h1_element = driver.find_element(By.CSS_SELECTOR, "h1")
@@ -45,8 +55,19 @@ try:
 
             print(f"Pobrano szczegóły dla elementu data-index={current_index}")
 
+            oferta = {
+                "id": if_offer,
+                "title": h1_element.text
+            }
+            data.append(oferta)
+            if_offer+= 1
+
+
             driver.back()
-            time.sleep(2)
+            time.sleep(random.uniform(1.5, 3.5))
+            element = driver.find_element(By.CSS_SELECTOR, "div")
+            ActionChains(driver).move_to_element_with_offset(element, random.randint(-10, 10), random.randint(-10, 10)).perform()
+            time.sleep(random.uniform(1, 3))
 
             current_index += 1
 
@@ -58,11 +79,17 @@ try:
 
             if current_index == last_index:
                 print("Koniec elementów na stronie.")
+                
                 break
             last_index = current_index
-
+    
 except Exception as e:
     print("Wystąpił błąd:", e)
+    with open('oferty_pracy.csv', mode='w', newline='', encoding='utf-8') as plik:
+        naglowki = ["id", "title"]
+        writer = csv.DictWriter(plik, fieldnames=naglowki)
+        writer.writeheader()
+        writer.writerows(data)
 
 finally:
     driver.quit()
